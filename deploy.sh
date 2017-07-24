08788ba6e23f392d31be26951370b9c2cbbf1055
Nathan Harkenrider <nathan.harkenrider@gmail.com>
2017-07-23 20:28:42 -0700

    Updating script to deploy to S3

--- deploy.sh
+++ deploy.sh
@@ -29,22 +29,17 @@ if [[ "$UPDATE_THEME" = true ]]; then
 fi
 
 # Build the site
-echo "Building site..."
-pushd ./public
-git pull
-popd
 hugo -t cocoa-hugo-theme
 
 # Commit changes
-pushd ./public
 git commit -a -m "$COMMIT_MSG"
 git push
-popd
 
-git commit -a -m "$COMMIT_MSG"
-git push
+# Deploying
+aws --profile blog-publisher --recursive cp public/ s3://www.nathanharkenrider.com
 
 echo "Done!"
+
 
 
 
