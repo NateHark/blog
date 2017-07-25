34b521510fdcba704b1682637eb6b009ae841b34
Nathan Harkenrider <nathan.harkenrider@gmail.com>
2017-07-24 21:43:12 -0700

    Fixing aws cli command line in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Deploying to S3..."
-aws --profile blog-publisher --recursive sync public/ s3://www.nathanharkenrider.com --delete
+aws --profile blog-publisher --delete s3 sync public/ s3://www.nathanharkenrider.com
