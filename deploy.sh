48269d349041291828b6f99064579d53a7699ea3
Nathan Harkenrider <nathan.harkenrider@gmail.com>
2017-07-25 21:27:28 -0700

    Adding Cloudfront invalidation to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+# Set environment variables if available
+if [[ -f set-env-vars.sh ]]; then
+    source set-env-vars.sh
+fi
+
 echo "Deploying to S3..."
 aws --profile blog-publisher --delete s3 sync public/ s3://www.nathanharkenrider.com
+
+echo "Invalidating Cloudfront distribution..."
+aws --profile blog-publisher cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths /\*
