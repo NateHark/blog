333478a45610cd227069fa414b40d6a2de8de52a
Nathan Harkenrider <nathan.harkenrider@gmail.com>
2017-08-23 20:48:56 -0700

    Additional static asset cache header fixes

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,12 @@ if [[ -f set-env-vars.sh ]]; then
     source set-env-vars.sh
 fi
 
-echo "Deploying to S3..."
-aws --profile blog-publisher s3 sync --delete --cache-control "max-age=2592000" public/ s3://www.nathanharkenrider.com
+echo "Deploying HTML assets to S3..."
+aws --profile blog-publisher s3 sync --delete --exclude "css/*" --exclude "img/*" public/ s3://www.nathanharkenrider.com
+echo "Deploying CSS to S3..."
+aws --profile blog-publisher s3 sync --delete --cache-control "max-age=2592000" public/img/ s3://www.nathanharkenrider.com/img
+echo "Deploying images to S3..."
+aws --profile blog-publisher s3 sync --delete --cache-control "max-age=2592000" public/css/ s3://www.nathanharkenrider.com/css
 
 echo "Invalidating Cloudfront distribution..."
 aws --profile blog-publisher cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths /\*
