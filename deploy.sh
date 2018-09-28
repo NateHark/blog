938b888c4a7aa11aa4b51c7e4d883987032f9561
Nathan Harkenrider <nathan.harkenrider@gmail.com>
2018-09-27 21:30:42 -0700

    Update target S3 bucket

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ if [[ -f set-env-vars.sh ]]; then
 fi
 
 echo "Deploying HTML assets to S3..."
-aws --profile blog-publisher s3 sync --delete --exclude "css/*" --exclude "img/*" public/ s3://www.nathanharkenrider.com
+aws --profile blog s3 sync --delete --exclude "css/*" --exclude "img/*" public/ s3://nathanharkenrider.com
 echo "Deploying CSS to S3..."
-aws --profile blog-publisher s3 sync --delete --cache-control "max-age=2592000" public/img/ s3://www.nathanharkenrider.com/img
+aws --profile blog s3 sync --delete --cache-control "max-age=2592000" public/img/ s3://nathanharkenrider.com/img
 echo "Deploying images to S3..."
-aws --profile blog-publisher s3 sync --delete --cache-control "max-age=2592000" public/css/ s3://www.nathanharkenrider.com/css
+aws --profile blog s3 sync --delete --cache-control "max-age=2592000" public/css/ s3://nathanharkenrider.com/css
 
 echo "Invalidating Cloudfront distribution..."
-aws --profile blog-publisher cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths /\*
+aws --profile blog cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths /\*
