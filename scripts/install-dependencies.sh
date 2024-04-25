449d495bdfdbdf9464b7a0ac3d6dcb6b3833a632
Nathan Harkenrider <nathan.harkenrider@gmail.com>
2024-04-25 06:13:22 +0000

    Adding devcontainers config

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HUGO_VERSION="0.111.3"
+
+curl -L -o hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
+
+tar -zxf hugo.tar.gz
+sudo mv hugo /usr/local/bin
+rm hugo.tar.gz
