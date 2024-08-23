aba0e5b12060452ac8a21d93c36245b3f69f03ea
Nathan Harkenrider <nathan.harkenrider@gmail.com>
2024-08-23 06:03:21 +0000

    Upgrade to Hugo 0.133.0

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HUGO_VERSION="0.111.3"
+HUGO_VERSION="0.133.0"
 
 curl -L -o hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
 
